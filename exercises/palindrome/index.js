b638a2055bd63efe842e2bd94ba2ca3c96b37220
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-09-27 20:24:17 +0800

    Complete palindome exercise
    Review every() method and performance trade-offs

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,41 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length-i-1]
+  })
+}
 
 module.exports = palindrome;
+
+/**
+ * My Solution #1: JS the Easy Way
+ */
+// function palindrome(str) {
+//   return str === str.split("").reverse().join("")
+// }
+
+/**
+ * My Solution #2: Interview Friendly Method
+ */
+// function palindrome(str) {
+//   let reversed = "";
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return str === reversed;
+// }
+
+/**
+ * Official Solution #1
+ */
+// function palindrome(str) {
+//   const reversed = str
+//     .split("")
+//     .reverse()
+//     .join("");
+
+//   return str === reversed;
+// }
