b6fa06618fb299117bd7fcb4eec5721be42b90c9
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-10-03 02:08:04 +0800

    Complete Anagram exercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,96 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+	return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+	return str
+		.replace(/[^\w]/g, "")
+		.toLowerCase()
+		.split("")
+		.sort()
+		.join("");
+}
+
+console.log(anagrams("One One", "Two two two"));
+
+/**
+ * My Solution #1: using JS array methods
+ */
+// function anagrams(stringA, stringB) {
+//   const sortedStrA = stringA.toLowerCase().replace(/[^a-z]/g, '').split('').sort().join('')
+//   const sortedStrB = stringB.toLowerCase().replace(/[^a-z]/g, '').split('').sort().join('')
+//
+//   return sortedStrA === sortedStrB
+// }
+
+// anagrams('RAIL! SAFETY!', 'fairy tales')
+
+/**
+ * My Solution #1a: cleaner RegEx w/ array methods
+ */
+// function anagrams(stringA, stringB) {
+//   const sortedStrA = stringA.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+//   const sortedStrB = stringB.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+//   return sortedStrA === sortedStrB
+// }
+
+/**
+ * Official Solution #1: Character Map approach
+ */
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   console.log("aCharMap", aCharMap);
+
+//   const bCharMap = buildCharMap(stringB);
+//   console.log("bCharMap", bCharMap);
+
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   for (let char in aCharMap) {
+//     console.log("for char: ", char)
+//     console.log(
+//       "aCharMap[char] & bCharMap[char]: ",
+//       aCharMap[char],
+//       bCharMap[char]
+//     );
+
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// function buildCharMap(str) {
+//   const charMap = {};
+
+//   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+
+//   return charMap;
+// }
+
+/**
+ * Official Solution #2: JS Methods with helper function
+ */
+// function anagrams(stringA, stringB) {
+//   return cleanString(stringA) === cleanString(stringB);
+// }
+
+// function cleanString(str) {
+//   return str
+//     .replace(/[^\w]/g, "")
+//     .toLowerCase()
+//     .split("")
+//     .sort()
+//     .join("");
+// }
 
 module.exports = anagrams;
