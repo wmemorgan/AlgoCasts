3efffcc30650d636d05955d00c08b8f7f6a9c139
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-10-05 00:05:56 +0800

    Complete Capitalization exercise

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,77 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // Create 'result' which is the first character of the input string capitalized
+  let result = `${str[0].toUpperCase()}`
+  // For each character in the string
+  for (let i = 1; i < str.length; i++) {
+    // IF the character to the left of a space
+    if (str[i - 1] === " ") {
+      // Capitalize it and add it to 'result'
+      result += str[i].toUpperCase()
+    } else {
+      //Add it to 'result'
+      result += str[i]
+    }    
+  }
+  console.log(`${str} result: `, result)
+  return result
+}
+
+capitalize("a short sentence");
 
 module.exports = capitalize;
+
+/**
+ * My Solution
+ */
+// function capitalize(str) {
+//   // Split string into arrays based on whitespace
+//   const newArr = str.split(" ").map(element => {
+//     // Capitalize the first letter in each element
+//     return `${element[0].toUpperCase()}${element.slice(1)}`;
+//   });
+//   console.log(`${str} converts to: `, newArr.join(" "));
+//   return newArr.join(" ");
+// }
+
+/**
+ * Official Solution #1
+ */
+// function capitalize(str) {
+//   // Make an empty array 'words'
+//   const words = []
+//   // Split the input string by space to get an array
+//   for (let word of str.split(' ')) {
+//     // For each word in the array
+//     // Uppercase the first letter of the word
+//     // Join first letter with rest of the string
+//     // Push result into 'words' array
+//     words.push(word[0].toUpperCase() + word.slice(1))
+//   }
+
+//   // Join 'words' into a string and return it
+//   return words.join(' ')
+// }
+
+/**
+ * Official Solution #2
+ */
+// function capitalize(str) {
+//   // Create 'result' which is the first character of the input string capitalized
+//   let result = `${str[0].toUpperCase()}`
+//   // For each character in the string
+//   for (let i = 1; i < str.length; i++) {
+//     // IF the character to the left of a space
+//     if (str[i - 1] === " ") {
+//       // Capitalize it and add it to 'result'
+//       result += str[i].toUpperCase()
+//     } else {
+//       //Add it to 'result'
+//       result += str[i]
+//     }
+//   }
+//   console.log(`${str} result: `, result)
+//   return result
+// }
