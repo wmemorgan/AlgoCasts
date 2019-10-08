30c241d01a7c84d53ae0f9728930c2a551ac66e1
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-10-08 11:24:35 +0800

    Complete Pyramid exercise

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,124 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = "") {
+	// If (row === n) (Base case)
+	if (n === row) {
+		return;
+	}
+
+	if (n * 2 - 1 === level.length) {
+		console.log(level);
+		pyramid(n, row + 1);
+		return;
+	}
+
+	const midpoint = Math.floor((n * 2 - 1) / 2);
+	let add;
+	if (midpoint - row <= level.length && midpoint + row >= level.length) {
+		add = "#";
+	} else {
+		add = " ";
+	}
+
+	pyramid(n, row, level + add);
+}
+
+pyramid(3);
 
 module.exports = pyramid;
+
+/**
+ * My Solution: (Attempted)
+ */
+// function pyramid(n) {
+//   // From 0 to n (iterate through rows)
+//   for (let row = 1; row <= n; row++) {
+//     // Create an empty string, 'level'
+//     let level = ''
+//     // From 1 to (n+2) (iterate through columns)
+//     for (let column = 1; column <= (n * 2 - 1); column++) {
+//       // IF the current column is equal to Math.floor((n+2)/2)
+//       if (column === Math.floor((n * 2 - 1) / 2)) {
+//         // Add a '#' to level
+//         level += '#'
+//       } else {
+//         // Add a space to 'level'
+//         level += " "
+//       }
+//     }
+//     // Console log 'level'
+//     console.log(level)
+//   }
+// }
+
+/**
+ * My Solution #1a: Iterative approach (with lots of help)
+ */
+// function pyramid(n) {
+//   // From 0 to n (iterate through rows)
+//   for (let row = 0; row < n; row++) {
+//     // Create an empty string, 'level'
+//     let level = "";
+//     // From 1 to (n+2) (iterate through columns)
+//     for (let column = 0; column < n * 2 - 1; column++) {
+//       // IF the current column is equal to Math.floor((n+2)/2)
+//       if (
+//         column >= Math.floor((n * 2 - 1) / 2) - row &&
+//         column <= Math.floor((n * 2 - 1) / 2) + row
+//       ) {
+//         // Add a '#' to level
+//         level += "#";
+//       } else {
+//         // Add a space to 'level'
+//         level += " ";
+//       }
+//     }
+//     // Console log 'level'
+//     console.log(level);
+//   }
+// }
+
+/**
+ * Official Solution #1: Iterative approach
+ */
+// function pyramid(n) {
+//   const midpoint = Math.floor((n * 2 - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = "";
+//     for (let column = 0; column < n * 2 - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+/**
+ * Official Solution #2: Recursive approach
+ */
+// function pyramid(n, row = 0, level = "") {
+// 	// If (row === n) (Base case)
+// 	if (n === row) {
+// 		return;
+// 	}
+
+// 	if (n * 2 - 1 === level.length) {
+// 		console.log(level);
+// 		pyramid(n, row + 1);
+// 		return;
+// 	}
+
+// 	const midpoint = Math.floor((n * 2 - 1) / 2);
+// 	let add;
+// 	if (midpoint - row <= level.length && midpoint + row >= level.length) {
+// 		add = "#";
+// 	} else {
+// 		add = " ";
+// 	}
+
+// 	pyramid(n, row, level + add);
+// }
