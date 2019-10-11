2bbc11e2ed5b61fd44f7412bb17dc02083ef88a4
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-10-11 18:07:29 +0800

    Complete 'Queue from Stacks' exercise

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,116 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.first = new Stack()
+    this.second = new Stack()
+  }
+
+  add(record) {
+    this.first.push(record)
+  }
+
+  remove() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop())
+    }
+    const record = this.second.pop()
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop())
+    }
+
+    return record
+  }
+
+  peek() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop())
+    }
+
+    const record = this.second.peek()
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop())
+    }
+
+    return record
+  }
+
+}
 
 module.exports = Queue;
+
+/**
+ * My Solution
+ */
+// class Queue {
+//   constructor() {
+//     this.stack1 = new Stack()
+//     this.stack2 = new Stack()
+//   }
+
+//   add(record) {
+//     while (this.stack2.peek()) {
+//       this.stack1.push(this.stack2.pop()) //Refill stack1
+//     }
+//     this.stack1.push(record)
+//   }
+
+//   remove() {
+//     while (this.stack1.peek()) {
+//       this.stack2.push(this.stack1.pop())
+//     }
+//     return this.stack2.pop()
+//   }
+
+//   peek() {
+//     while (this.stack1.peek()) {
+//       this.stack2.push(this.stack1.pop())
+//     }
+//     return this.stack2.peek()
+//   }
+// }
+
+/**
+ * Official Solution
+ */
+// class Queue {
+//   constructor() {
+//     this.first = new Stack()
+//     this.second = new Stack()
+//   }
+
+//   add(record) {
+//     this.first.push(record)
+//   }
+
+//   remove() {
+//     while (this.first.peek()) {
+//       this.second.push(this.first.pop())
+//     }
+//     const record = this.second.pop()
+
+//     while (this.second.peek()) {
+//       this.first.push(this.second.pop())
+//     }
+
+//     return record
+//   }
+
+//   peek() {
+//     while (this.first.peek()) {
+//       this.second.push(this.first.pop())
+//     }
+
+//     const record = this.second.peek()
+
+//     while (this.second.peek()) {
+//       this.first.push(this.second.pop())
+//     }
+
+//     return record
+//   }
+
+// }
