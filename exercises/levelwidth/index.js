4cbf134ccbebaaeb483b7862eb97537c2141b78d
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-10-15 12:20:31 +0800

    Complete Level Width exercise

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,69 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+	const arr = [root, "s"];
+	const counters = [0];
+
+	while (arr.length > 1) {
+		const node = arr.shift();
+
+		if (node === "s") {
+			counters.push(0);
+			arr.push("s");
+		} else {
+			arr.push(...node.children);
+			counters[counters.length - 1]++;
+		}
+	}
+
+	return counters;
+}
 
 module.exports = levelWidth;
+
+/**
+ * My Solution
+ */
+// function levelWidth(root) {
+//   const counters = [0];
+//   const arr = [root, "s"];
+
+//   while (arr.length > 1) {
+//     if (arr[0] === "s") {
+//       arr.shift();
+//       arr.push("s");
+//       counters.push(0);
+//     }
+
+//     const node = arr.shift();
+//     if (node.children) {
+//       arr.push(...node.children)
+//     }
+
+//     counters[counters.length - 1]++;
+//   }
+
+//   return counters;
+// }
+/**
+ * Official Solution
+ */
+// function levelWidth(root) {
+//   const arr = [root, "s"];
+//   const counters = [0];
+
+//   while (arr.length > 1) {
+//     const node = arr.shift();
+
+//     if (node === "s") {
+//       counters.push(0);
+//       arr.push("s");
+//     } else {
+//       arr.push(...node.children);
+//       counters[counters.length - 1]++;
+//     }
+//   }
+
+//   return counters;
+// }
