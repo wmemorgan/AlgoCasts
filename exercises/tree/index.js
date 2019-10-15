75b534bb45d9cdf8d105869e253e2f58e390ea81
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-10-15 11:22:51 +0800

    Complete Tree exercise

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,71 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.children = [];
+	}
 
-class Tree {}
+	add(data) {
+		this.children.push(new Node(data));
+	}
+
+	remove(data) {
+		this.children = this.children.filter(node => node.data !== data);
+	}
+}
+
+class Tree {
+	constructor() {
+		this.root = null;
+	}
+
+	traverseBF(fn) {
+		const arr = [this.root];
+		while (arr.length) {
+			const node = arr.shift();
+			// Use queue data structure
+			arr.push(...node.children);
+			fn(node);
+		}
+	}
+
+	traverseDF(fn) {
+		const arr = [this.root];
+		while (arr.length) {
+			const node = arr.shift();
+			// Use stack data structure
+			arr.unshift(...node.children);
+			fn(node);
+		}
+	}
+}
 
 module.exports = { Tree, Node };
+
+/**
+ * traverseBF: My Solution
+ */
+// traverseBF(fn) {
+//   const arr = [this.root]
+//   while (arr.length > 0) {
+//     const firstElem = arr.shift()
+//     for (let el of firstElem.children) {
+//       arr.push(el)
+//     }
+//     fn(firstElem)
+//   }
+// }
+
+/**
+ * traverseBF: Official Solution
+ */
+// traverseBF(fn) {
+//   const arr = [this.root]
+//   while (arr.length) {
+//     const node = arr.shift()
+//     arr.push(...node.children)
+//     fn(node)
+//   }
+// }
